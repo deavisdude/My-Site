4683131370ecde28a259d725bc337a49fa0a78fc
Davis Odom <deavisdude@gmail.com>
2015-06-03 21:50:58 -0400

    favicon and stuff

--- send.php
+++ send.php
@@ -2,7 +2,7 @@
 
 $to      = 'me@davisodom.com';
 $subject = 'One of your visitors sent you a message!';
-$body    = $_POST['message'];
+$body    = "Name: ".$_POST['name']."\n".$_POST['message'];
 $headers = "From:" . $_POST['email'];
 
 mail($to, $subject, $body, $headers);
